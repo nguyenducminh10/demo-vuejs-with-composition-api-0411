5c8330a33bd56a6315c1baa3b8728ebe8de66372
nguyenducminh10 <44760308+nguyenducminh10@users.noreply.github.com>
2021-11-04 14:41:32 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <TodoApp msg="Welcome to Your Vue.js App"/>
+  <p>--------------------------------------</p>
+  <todo-app-with-composition/>
+</template>
+
+<script>
+import TodoApp from './components/TodoApp.vue'
+import TodoAppWithComposition from './components/TodoAppWithComposition.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoApp,
+    TodoAppWithComposition
+  },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <input v-model="task"/>
+        <button v-on:click="submitTask">Submit</button>
+        <div v-show="isShowHideText">
+            <p>Helloworld</p>
+        </div>
+       
+        <div v-for="(task, index) in tasks" :key="index">
+          <td>{{task.name}}</td>
+          <td>{{task.status}}</td>
+          <td @click="deleteTask">x</td>
+          <td ></td>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoApp',
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      isShowHideText: true,
+      task:'',
+      tasks: [
+        {
+          name: 'Hello world 1',
+          status: 'to-do'
+        },
+          {
+          name: 'Hello world 1',
+          status: 'in-progress'
+        }
+      ]
+    }
+  },
+  methods: {
+    submitTask(){
+      if(this.task.length === 0) return ;
+      this.tasks.push({
+        name:this.task,
+        status: 'to-do'
+      })
+      this.task = ''
+    },
+    deleteTask(index){
+      this.tasks.splice(index, 1)
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
